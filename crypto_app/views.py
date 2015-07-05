49fc41aa553aa3856b129f11311e8cade9692721
andela-kanyanwu <kosy.anyanwu@andela.co>
2015-07-05 17:41:52 +0100

    chore/code cleanup, cancel form resubmission

--- crypto_app/views.py
+++ crypto_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import View
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 
 def crypt_encode_decode(data, key):
@@ -27,26 +29,34 @@ def hex_to_string(hex_value):
 # Create your views here.
 class HomeView(View):
     def get(self, request):
-        return render(request, 'index.html')
+        decoded_data = request.session.get('decoded_data', "")
+        encoded_data = request.session.get('encoded_data', "")
+        key = request.session.get('key_to_encode', "")
+        request.session.clear()
+        return render(request, 'index.html',
+                       {"decoded_data": decoded_data,
+                        "encoded_data": encoded_data,
+                        "key": key}
+                      )
 
     def post(self, request):
         data = request.POST.get('data', False)
         key = request.POST.get('key', False)
         encoded_data = request.POST.get('encoded_data', False)
 
+        request.session['key_to_encode'] = str(key)
+
         #To encode
         if data and key:
             data_to_encode = str(data)
-            key_to_encode = str(key)
-            encoded_data_buffer = crypt_encode_decode(data_to_encode, key_to_encode)
+            encoded_data_buffer = crypt_encode_decode(data_to_encode, str(key))
             hex_encoded_data = string_to_hex(encoded_data_buffer)
-
-            return render(request, 'index.html', {"encoded_data": hex_encoded_data, "key": key_to_encode})
+            request.session['encoded_data'] = hex_encoded_data
+            return HttpResponseRedirect(reverse('home'))
 
         #To decode
         if encoded_data and key:
             encoded_data_str = hex_to_string(str(encoded_data))
-            key_to_encode = str(key)
-            decoded_data_buffer = crypt_encode_decode(encoded_data_str, key_to_encode)
-
-            return render(request, 'index.html', {"decoded_data": decoded_data_buffer, "key": key_to_encode})
+            decoded_data_buffer = crypt_encode_decode(encoded_data_str, str(key))
+            request.session['decoded_data'] = decoded_data_buffer
+            return HttpResponseRedirect(reverse('home'))
